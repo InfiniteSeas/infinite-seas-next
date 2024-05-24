7769a7f9a795457ed4226f4c86092e45c0b4911a
Archimesons <7592301+archimesons@user.noreply.gitee.com>
2024-05-24 19:46:25 +0800

    finish rebuild and ui

--- components/menus/ShipsMenu.tsx
+++ components/menus/ShipsMenu.tsx
@@ -28,14 +28,18 @@ export default function ShipsMenu({ closeShipsMenu }: { closeShipsMenu: () => vo
   ];
 
   return (
-    <div className="fixed top-0 left-0 w-2/3 h-screen flex flex-col justify-between ml-[15%] mr-[20%]">
-      <div className="w-full h-4/5 flex border-[1px] border-white">
-        <div className="w-full h-full border-[1px] border-red-400">
-          <div className="w-full h-[10%] flex justify-around items-center border-[1px] border-black">
+    <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-2/3 h-screen flex justify-center items-center">
+      {!shipsBagBackg && (
+        <div className="w-full relative flex flex-col bg-[#343A40] p-12 gap-4">
+          <div className="absolute top-0 right-4 text-6xl text-white cursor-pointer" onClick={closeShipsMenu}>
+            ×
+          </div>
+
+          <div className="w-full flex justify-around items-center py-4">
             {shipsTopData.map((data) => (
               <div
                 key={data.id}
-                className="h-full flex items-center bg-contain text-white text-3xl cursor-pointer bg-btn-frame"
+                className="w-64 h-16 flex justify-center items-center text-white text-3xl cursor-pointer bg-btn-frame bg-center bg-no-repeat bg-cover"
               >
                 {data.title}
               </div>
@@ -44,11 +48,11 @@ export default function ShipsMenu({ closeShipsMenu }: { closeShipsMenu: () => vo
 
           {ships.map((ship) => (
             <div key={ship.id} className="w-full">
-              <div className="w-full h-[125px] flex items-center bg-black">
-                <div className="relative w-1/5 h-full border-[1px] border-white">
+              <div className="w-full h-[125px] flex items-center bg-black gap-4">
+                <div className="relative w-1/6 h-full">
                   <div className="absolute left-1 text-white text-xl">{ship.id}</div>
                   <Image
-                    className="w-[70%] h-4/5 absolute top-1/2 left-1/2 -transport-x-1/2 -transport-y-1/2"
+                    className="w-4/5 h-4/5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
                     src="/image/Ocean/Boat_big.png"
                     alt="boat-big"
                     width={125}
@@ -57,14 +61,14 @@ export default function ShipsMenu({ closeShipsMenu }: { closeShipsMenu: () => vo
                   />
                 </div>
 
-                <div className="w-4/5 h-full text-white ml-2">
+                <div className="flex-1 h-full text-white border-x-[1px] border-white p-4">
                   <p>Name: {ship.name}</p>
                   <p>ID: {ship.ID}</p>
                   <p>HP: {ship.HP}</p>
                 </div>
 
                 <div
-                  className="w-1/5 h-full flex justify-center items-center text-white text-2xl border-[1px] border-white cursor-pointer"
+                  className="w-1/5 h-full flex justify-center items-center text-white text-2xl cursor-pointer"
                   onClick={() => setShipsBagBackg(true)}
                 >
                   TransFer Inventory
@@ -73,46 +77,64 @@ export default function ShipsMenu({ closeShipsMenu }: { closeShipsMenu: () => vo
             </div>
           ))}
 
-          <div className="w-full h-[10%] flex justify-center items-center border-[1px] border-white">
-            <div className="w-[90px] h-[100px] text-4xl text-center text-white cursor-pointer bg-cotain bg-btn-frame">
+          <div className="w-full flex justify-end items-center gap-4">
+            <div className="w-[90px] aspect-[1] text-center text-7xl text-white cursor-pointer bg-btn-frame bg-center bg-no-repeat bg-cover">
               +
             </div>
-          </div>
 
-          <div className="absolute top-6 right-0 text-3xl cursor-pointer" onClick={closeShipsMenu}>
-            ×
+            <div className="w-[144px] h-[70px] flex justify-center items-center text-white text-3xl cursor-pointer bg-btn-frame bg-center bg-no-repeat bg-cover">
+              SAIL
+            </div>
           </div>
-
-          <div className="absolute right-6 w-[144px] h-[77px] text-center text-white text-3xl bg-btn-frame">SAIL</div>
         </div>
-      </div>
+      )}
 
       {shipsBagBackg && (
-        <div className="flex justify-between items-center border-[1px] border-white h-full w-[115%]">
+        <div className="flex justify-between items-center h-full w-[115%]">
           {shipInventories.map((inventory) => (
-            <div key={inventory.id} className="flex flex-col items-center w-[26%] h-[80%] border-[1px] border-black">
-              <div className="flex justify-evenly items-center border-[1px] border-white w-full h-[15%]">
-                <div className="w-[90px] h-[99px] bg-btn-frame bg-contain">
-                  <Image className="w-full h-full scale-[60%]" src={inventory.iconUrl} alt="inventory-pic" />
+            <div key={inventory.id} className="flex flex-col items-center w-[26%] h-[80%] bg-[#343A40]">
+              <div className="flex justify-evenly items-center w-full h-[15%]">
+                <div className="w-[120px] h-[120px] bg-btn-frame bg-center bg-no-repeat bg-cover">
+                  <Image
+                    className="w-full h-full scale-[60%]"
+                    src={inventory.iconUrl}
+                    alt="inventory-pic"
+                    width={120}
+                    height={120}
+                    priority
+                  />
                 </div>
-                {[0, 1, 2, 3, 4, 5, 6, 7].map((num) => (
-                  <div key={num} className="w-[180px] h-[56px] bg-btn-frame bg-contain text-white text-center">
+
+                <div className="flex justify-center items-center bg-btn-frame bg-center bg-no-repeat bg-cover text-white text-center px-12 py-3">
+                  {inventory.name}
+                </div>
+
+                {/* {[0, 1, 2, 3, 4, 5, 6, 7].map((num) => (
+                  <div
+                    key={num}
+                    className="w-[180px] h-[56px] bg-btn-frame bg-center bg-no-repeat bg-cover text-white text-center"
+                  >
                     {inventory.name}
                   </div>
-                ))}
+                ))} */}
               </div>
 
-              <div className="w-[85%] flex flex-col items-center bg-[#fffbe7] px-4 py-3">
-                <div className="w-[270px] h-[72px] text-2xl text-white text-center bg-btn-frame">SHIP INVENTORY</div>
-                <div className="grid grid-cols-2 gap-3">
-                  <div className="w-[90px] h-[99px] bg-btn-frame bg-contain" />
+              <div className="w-[85%] flex flex-col items-center bg-[#fffbe7] p-4 gap-4">
+                <div className="text-xl text-white text-center bg-btn-frame bg-center bg-no-repeat bg-cover px-16 py-3">
+                  SHIP INVENTORY
+                </div>
+
+                <div className="w-full grid grid-cols-3 gap-3">
+                  {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
+                    <div key={num} className="w-full aspect-[1] bg-btn-frame bg-center bg-no-repeat bg-cover" />
+                  ))}
                 </div>
               </div>
             </div>
           ))}
 
           <div
-            className="w-[135px] h-[72px] absolute bottom-1 right-5 text-white text-center text-3xl cursor-pointer bg-btn-frame bg-contain"
+            className="w-[135px] h-[72px] absolute bottom-10 right-0 text-white flex justify-center items-center text-3xl cursor-pointer bg-btn-frame bg-center bg-no-repeat bg-cover"
             onClick={() => setShipsBagBackg(false)}
           >
             back
