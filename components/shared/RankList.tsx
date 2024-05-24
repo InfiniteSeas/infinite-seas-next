e5c05b778d23ae98836794bc3409b31963ffd99a
Archimesons <7592301+archimesons@user.noreply.gitee.com>
2024-05-24 14:43:40 +0800

    fix rank list visual problem

--- components/shared/RankList.tsx
+++ components/shared/RankList.tsx
@@ -1,3 +1,3 @@
 export default function RankList() {
-  return <div className="absolute top-1/2 -right-1/3 -translate-y-1/2 w-[240px] height-[460px] bg-ranking-text" />;
+  return <div className="absolute top-1/2 -right-[36%] -translate-y-1/2 w-[240px] h-[420px] bg-ranking-text bg-center bg-no-repeat bg-cover" />;
 }
